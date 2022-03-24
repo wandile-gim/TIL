614f5ac6632d44f4f20d7e9260b3ab67b058e32f
wonjae <dnjswo6253@naver.com>
2022-03-25 00:58:30 +0900

    programmers

--- Algorithm/백준 단계별/동적계획법/1932.py
+++ Algorithm/백준 단계별/동적계획법/1932.py
@@ -5,7 +5,6 @@ sys.stdin = open(path, "r")
 N = int(sys.stdin.readline())
 triangle = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-way = 0
 for i in range(1, len(triangle)):
     for j in range(i+1):
         if j == 0:
@@ -13,8 +12,7 @@ for i in range(1, len(triangle)):
         elif i == j:
             triangle[i][j] = triangle[i][j] + triangle[i-1][j-1]
         else:
-            print(i, j)
-            triangle[i][j] = max(triangle[i][j] + triangle[i-1]
-                                 [j], triangle[i][j] + triangle[i-1][j-1])
+            triangle[i][j] = max(triangle[i][j] + triangle[i-1][j],
+                                 triangle[i][j] + triangle[i-1][j-1])
 
 print(max(triangle[-1]))
