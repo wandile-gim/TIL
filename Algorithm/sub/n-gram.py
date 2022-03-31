d90e8f32fb7444b4f3ca4b3a05c0d5a52138e23a
wonjae <dnjswo6253@naver.com>
2022-04-01 00:00:13 +0900

    sub

--- Algorithm/sub/n-gram.py
+++ Algorithm/sub/n-gram.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from os import sep
 
 
@@ -12,7 +13,26 @@ text = 'hello'
 # for i in range(len(text)-2):
 #     print(text[i], text[i+1], text[i+2], sep='')
 
-print(list(zip(*['hello', 'ello', 'llo'])))
-# text = 'show me the money'
-# words = text.split()
-# print(list(zip(words, words[1:])))
+# print(list(zip(*['hello', 'ello', 'llo'])))
+
+# print(list(zip(*[text[i:] for i in range(3)])))
+# # text = 'show me the money'
+# # words = text.split()
+# # print(list(zip(words, words[1:])))
+# word = 'level'
+
+# print(word == ''.join(reversed(word)))
+
+# ho = True
+# for i in range(len(word)//2):
+#     if word[i] != word[-1-i]:
+#         ho = False
+#         break
+# print(ho)
+
+
+a = [5, 7, 8, 12, 2, 4, 0, 3, 9]
+print([i for i in a if i > 5 and i < 10])
+print(list(filter(lambda x: x > 5 and x < 10, a)))
+a = [1, 2, 3, 4, 5]
+print(reduce(lambda x, y: x+y, a))
